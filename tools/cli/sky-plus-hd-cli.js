1baa13199da237cded0023c7bf3581da0b167e88
przemyslawpluta <przemekpluta@hotmail.com>
2014-07-04 09:35:53 +0100

    update
    cleanup and fixes
    
    extend cli options

--- tools/cli/sky-plus-hd-cli.js
+++ tools/cli/sky-plus-hd-cli.js
@@ -1,91 +1,170 @@
-var celeri = require('celeri');
-var util = require('util');
-var SkyPlusHD = require('../..');
+var celeri = require('celeri'),
+   SkyPlusHD = require('../..'),
+   skyFinder = new SkyPlusHD().find();
 
+skyFinder.then(function find(skyBox) {
 
-var skyFinder = new SkyPlusHD().find();
+   'use strict';
 
-skyFinder.then(function(skyBox) {
-   console.log("READY: "+skyBox.description);
+   console.log('Initialized: ' + skyBox.description);
 
-   celeri.open({  
-       prefix: 'sky-plus-hd > '
+   skyBox.on('ready', function ready() {
+      console.log('Ready ...');
+      celeri.open({ prefix: 'sky-plus-hd > '});
    });
 
    celeri.option({
-      command: "channel :number",
-      description: "Changes to the specified channel number",
-   },function(data) {
-      var spinner = celeri.loading("Changing to channel "+data.number);
-      skyBox.setChannel({number:parseInt(data.number)}).then(function() {
+      command: 'channel :number',
+      description: 'Changes to the specified channel number',
+   }, function resolve(data) {
+      var spinner = celeri.loading('Changing to channel ' + data.number);
+
+      skyBox.setChannel({number:parseInt(data.number, 10)}).then(function resolve() {
          spinner.done();
-      }).fail(function() {
+      }).fail(function fail() {
          spinner.done(true);
       });
+
    });
 
    celeri.option({
       command: 'pause',
-      description: "Pauses the currently playing programme",
-   },function() {
-      var spinner = celeri.loading("Pausing");
-      skyBox.pause().then(function() {
+      description: 'Pauses the currently playing programme',
+   }, function resolve() {
+      var spinner = celeri.loading('Pausing');
+
+      skyBox.pause().then(function pause() {
          spinner.done();
-      }).fail(function() {
+      }).fail(function fail() {
          spinner.done(true);
       });
-      skyBox.pause()
+
+   });
+
+   celeri.option({
+      command: 'play',
+      description: 'Play the currently playing programme',
+   }, function resolve() {
+      var spinner = celeri.loading('Playing');
+
+      skyBox.play().then(function play() {
+         spinner.done();
+      }).fail(function fail() {
+         spinner.done(true);
+      });
+
+   });
+
+   celeri.option({
+      command: 'fwd',
+      description: 'Fwd the currently playing programme',
+   }, function resolve() {
+      var spinner = celeri.loading('Playing');
+
+      skyBox.fwd().then(function fwd() {
+         spinner.done();
+      }).fail(function fail() {
+         spinner.done(true);
+      });
+
+   });
+
+   celeri.option({
+      command: 'rew',
+      description: 'Rew the currently playing programme',
+   }, function resolve() {
+      var spinner = celeri.loading('Playing');
+
+      skyBox.rew().then(function rew() {
+         spinner.done();
+      }).fail(function fail() {
+         spinner.done(true);
+      });
+
+   });
+
+   celeri.option({
+      command: 'up',
+      description: 'Channel up from currently playing programme',
+   }, function resolve() {
+      var spinner = celeri.loading('Playing');
+
+      skyBox.channelUp().then(function channelUp() {
+         spinner.done();
+      }).fail(function fail() {
+         spinner.done(true);
+      });
+
+   });
+
+   celeri.option({
+      command: 'down',
+      description: 'Channel down from currently playing programme',
+   }, function resolve() {
+      var spinner = celeri.loading('Playing');
+
+      skyBox.channelDown().then(function channelDown() {
+         spinner.done();
+      }).fail(function fail() {
+         spinner.done(true);
+      });
+
    });
 
    celeri.option({
       command: 'channelinfo :channel',
-      description: "Show info for the specified channel"
-   },function(data) {
-      var spinner = celeri.loading("Getting channel info for "+data.channel);
-      skyBox.findChannel({number:+data.channel}).then(function(channel) {
+      description: 'Show info for the specified channel'
+   },function resolve(data) {
+      var spinner = celeri.loading('Getting channel info for ' + data.channel);
+
+      skyBox.findChannel({number: +data.channel}).then(function resolve(channel) {
          spinner.done();
          var table = [channel];
-         table.push({number:'======',name:'====',id:'==',idHex:'======'},{number:'NUMBER',name:'NAME',id:'ID',idHex:'ID HEX'});
-         celeri.drawTable(table,{
-            columns: ['number','name','id','idHex']
-         });
-      }).fail(function(err){
+         table.push({number:'======', name:'====', id:'==', idHex:'======'},{number:'NUMBER', name:'NAME', id:'ID', idHex:'ID HEX'});
+         celeri.drawTable(table,{ columns: ['number', 'name', 'id', 'idHex'] });
+      }).fail(function fail(){
+         spinner.done(true);
+      });
+
+   });
+
+   celeri.option({
+      command: 'channels',
+      description: 'Show info for all channels'
+   },function resolve() {
+      var spinner = celeri.loading('Getting channels list');
+      skyBox.getChannelList().then(function resolve(channelList) {
+         channelList.reverse();
+
+         spinner.done();
+         celeri.drawTable(channelList,{ columns: ['number','name','id','idHex'] });
+      }).fail(function fail() {
          spinner.done(true);
       });
    });
 
    celeri.option({
-      command: 'channels',
-      description: "Show info for all channels"
-   },function(data) {
-      var spinner = celeri.loading("Getting channels list");
-      skyBox.getChannelList().then(function(channelList) {
-         channelList.reverse();
-         
+      command: 'planner',
+      description: 'Show info for planner items'
+   },function resolve() {
+      var spinner = celeri.loading('Getting planner list');
+      skyBox.planner.getPlannerItems().then(function resolve(items) {
+         items.reverse();
          spinner.done();
-         celeri.drawTable(channelList,{
-            columns: ['number','name','id','idHex']
-         });
-      }).fail(function(err) {
+         celeri.drawTable(items, { columns: ['title', 'isViewed', 'description'] });
+      }).fail(function fail() {
          spinner.done(true);
-      })
-   })
+      });
+   });
 
    celeri.parse(process.argv);
 
-   /*
-   console.log("Reading planner...");
-   skyBox.planner.getPlannerItems().then(function(items) {
-      console.log('Planner contains '+items.length + ' items');
-   });
-   */
-   /*
-   skyBox.on('stateChanged',function(playEvent) {
-      console.log(util.format(">>> State:[%s] URI:[%s] Speed:[%s]",playEvent.TransportState,playEvent.CurrentTrackURI,playEvent.TransportPlaySpeed));
-   });
-   */
 });
 
-skyFinder.fail(function(err) {
-   console.log("Failed to find skybox, "+err);
+skyFinder.fail(function fail(err) {
+
+   'use strict';
+
+   console.log('Failed to find skybox, ' + err);
+
 });
--- tools/monitor/sky-plus-hd-monitor.js
+++ tools/monitor/sky-plus-hd-monitor.js
@@ -1,22 +1,36 @@
-var util = require('util');
-var SkyPlusHD = require('../..');
+var util = require('util'),
+   SkyPlusHD = require('../..'),
+   skyFinder = new SkyPlusHD().find();
 
-var skyFinder = new SkyPlusHD().find();
+skyFinder.then(function find(skyBox) {
 
-skyFinder.then(function(skyBox) {
-   console.log("READY: "+skyBox.description);
+   'use strict';
 
-   /*
-   console.log("Reading planner...");
-   skyBox.planner.getPlannerItems().then(function(items) {
-      console.log('Planner contains '+items.length + ' items');
+   console.log('Initialized: ' + skyBox.description);
+
+   console.log('Reading planner...');
+
+   skyBox.planner.getPlannerItems().then(function getPlannerItems(items) {
+      console.log('Planner contains ' + items.length + ' items');
    });
-   */
-   skyBox.on('stateChanged',function(playEvent) {
-      console.log(util.format(">>> State:[%s] URI:[%s] Speed:[%s]",playEvent.TransportState,playEvent.CurrentTrackURI,playEvent.TransportPlaySpeed));
+
+   skyBox.on('stateChanged', function stateChanged(playEvent) {
+      console.log(util.format('>>> State:[%s] URI:[%s] Speed:[%s]', playEvent.TransportState, playEvent.CurrentTrackURI, playEvent.TransportPlaySpeed));
    });
+
+   skyBox.on('ready', function ready(channel) {
+
+      console.log('Ready ...');
+      console.dir(channel);
+
+   });
+
 });
 
-skyFinder.fail(function(err) {
-   console.log("Failed to find skybox, "+err);
+skyFinder.fail(function fail(err) {
+
+   'use strict';
+
+   console.log('Failed to find skybox, ' + err);
+
 });
